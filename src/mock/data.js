226504157c790aafe6791ec48d0866d35ebac5f6
PatriciaWhitezip <patcostalonga@gmail.com>
2021-07-26 17:30:23 +0100

    Style and text updates

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.png',
-  paragraphOne: 'I\'m a creative Front-end Developer with a diverse Design background. I have the ability to take up new technical concepts quickly and upskill in different coding practices. I have extensive experience in developing and refining products translating company brand values.',
+  paragraphOne: 'I\'m a creative Front-end Developer with a diverse Design background. I have the ability to take up new technical concepts quickly and upskill in different coding practices. I have extensive experience in developing and refining products translating company brand values. I have a valid visa to work in the UK.',
   // paragraphTwo: '',
   // paragraphThree: '',
   resume: 'https://patriciacostalonga.github.io/CV/', // if no resume, the button will not show up
@@ -28,30 +28,48 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
+    img: 'Huikee_desktop.jpg',
+    title: 'Huikee\'s Website',
+    info: 'HMW: build a pay-n-play Finnish online casino in three months',
+    info2: 'I was in a group of four people, including a team leader, and our challenge was to build a pay-n-play Finnish online casino in three months. We worked in developing the logo, style guide, meetings with stakeholders till the coding of the pages. It was an incredible project because I could rely a lot on teamwork, creativity and explore new directions in coding, especially with animation. In this project, I used Figma, Photoshop, HTML, PHP, CSS/SASS, Javascript and JQuery. To access the link, you\'ll need to be in Finland or use a VPN.',
+    url: 'https://www.huikee.com/en-fi/vip/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
     img: 'CS_reg_light_mode_01.jpg',
-    title: 'Casimba register Page',
+    title: 'Casimba Registration Page',
     info: 'HMW: improve trust signals on Casimba\'s website and increasing conversion',
-    info2: 'I was responsible for translating the new style on the Registration Page. It was the first time I worked with animation on Angular and I set up a dark/light mode switch that worked through stored cookies and also when the switch button was clicked. During the process I made sure to follow the same dimensions, font-sizes, colours and animation style from the main website to keep the concept across all pages. Recently I added an easier step for filling out the form through the Trustly button that doesn\'t required so many details from the user. This  shorter option is only available for some players from specific jurisdictions.',
+    info2: 'I was responsible for translating the new style on the Registration Page. It was the first time I worked with animation on Angular and I set up a dark/light mode switch that worked through stored cookies and also when the switch button was clicked. During the process, I made sure to follow the same dimensions, font sizes, colours and animation style from the main website to keep the concept across all pages. Recently I added an easier step for filling out the form through the Trustly button that doesn\'t require so many details from the user. This shorter option is only available for some players from specific jurisdictions. In this project, I used Figma, Photoshop and Angular.',
     url: 'https://www.casimba.com/create-account/personal-details',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'Templenile_desktop.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://www.templenile.com/create-account/personal-details', // if no repo, the button will not show up
+    img: 'Jackpotvillage_desktop.jpg',
+    title: 'Jackpot Village Registration Page',
+    info: 'HMW: improve trust signals on Jackpot Village\'s website and increasing conversion',
+    info2: 'I was responsible for updating the Registration page on Jackpot Village to attract more users and to translate the style of the website to it. It was the first time I changed a bit more the structure of the registration page. In this project, I used Figma, Photoshop and Angular.',
+    url: 'https://www.jackpotvillage.com/create-account/personal-details',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'Jackpotvillage_desktop.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://www.jackpotvillage.com/create-account/personal-details', // if no repo, the button will not show up
+    img: 'Templenile_desktop.jpg',
+    title: 'Temple Nile Registration Page',
+    info: 'HMW: improve trust signals on Jackpot Village\'s website and increasing conversion',
+    info2: 'I was responsible for updating the Registration page on Temple Nile to increase user conversion and translate the style of the main website on it. In this project, I used Figma, Photoshop and Angular.',
+    url: 'https://www.templenile.com/create-account/personal-details',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Dreamvegas_desktop.jpg',
+    title: 'Dream Vegas Registration Page',
+    info: 'HMW: improve trust signals on Dream Vegas\'s website and increasing conversion',
+    info2: 'I was responsible for updating the Registration page on Dream Vegas to increase user conversion and translate the style of the main website on it. This was my first project working with Angular. In this project, I used Figma, Photoshop and Angular.',
+    url: 'https://www.dreamvegas.com/create-account/personal-details',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
